808076b39734f1cf94252602a8d0291ad5f5a03d
vasyan <vasyan.coder@gmail.com>
2024-07-05 17:36:26 +0300

    init

---
+++ src/main/java/org/mywish/mywishserver/MyWishServerApplication.java
@@ -0,0 +1,13 @@
+package org.mywish.mywishserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWishServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWishServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mywish/mywishserver/MyWishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mywish.mywishserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWishServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
