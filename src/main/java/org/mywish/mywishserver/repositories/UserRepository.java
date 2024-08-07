9a1953953747d911a22cd21099b1cab46a70746a
vasyan <vasyan.coder@gmail.com>
2024-08-07 12:49:45 +0300

    ref: change configuration

--- src/main/java/org/mywish/mywishserver/repositories/UserRepository.java
+++ src/main/java/org/mywish/mywishserver/repositories/UserRepository.java
@@ -4,5 +4,8 @@ package org.mywish.mywishserver.repositories;
 import org.mywish.mywishserver.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
